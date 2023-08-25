dc7047c0cbca3872770e922baae59f97f201d90e
Jan Dyndor <dyndorjan@gmail.com>
2023-08-25 14:02:03 +0200

    updating readme day 1, and doing day 2

---
+++ Day 2/Exercises.py
@@ -0,0 +1,34 @@
+#
+#  ! Exercise 1 - Data Types
+# 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# # Write your code below this line 👇
+# first_digit = int(two_digit_number[:1])
+# second_digit = int(two_digit_number[1:])
+# print(first_digit + second_digit)
+
+# ! Exercise 2 - BMI Calculator
+# # 🚨 Don't change the code below 👇
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# BMI = int(weight/height**2)
+# # print(f"{weight} / ({height} x {height}) = {BMI}")
+# print(BMI)
+
+# ! Exercise 3 - Life in Weeks (
+# # 🚨 Don't change the code below 👇
+# age = int(input("What is your current age? "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# days= (90*365)-(age*365)
+# weeks = (90*52)-(age *52)
+# months = (90*12)-(age * 12)
+
+# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
---
+++ Day 2/ReadMe.md
@@ -0,0 +1,12 @@
+Type Error, Type Checking and Type Conversion 24. Number Manipulation and F Strings in Python
+
+# Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings
+
+## What I have learned?
+
+- Python primitive data types
+- Type error, type checking and conversion
+- Math operations
+- F strings
+
+## [I have also completed a Day 2 project - Tip Calculator](https://replit.com/@Jan-Dyndor/tip-calculator-start)
