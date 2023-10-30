e05281e049100db2ce4dfae1ecaad9218c18f4c0
Jan Dyndor <dyndorjan@gmail.com>
2023-10-30 23:04:58 +0100

    Day 22

--- Day 23/car_manager.py
+++ Day 23/car_manager.py
@@ -3,7 +3,7 @@ import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+CAR_LIST = list()
 
 class CarManager(Turtle):
     def __init__(self):
@@ -14,24 +14,21 @@ class CarManager(Turtle):
         self.goto(260, random.randint(-250, 250))
         self.seth(180)
         self.shape("square")
-        self.move_increment = 10
-        self.starting_distance = 5
+        # self.move_increment = 10
+        # self.starting_distance = 5
+        CAR_LIST.append(self)
+
 
 
 
     def move_car(self):
-        self.forward(self.starting_distance)
-        print(self.starting_distance)
-        self.starting_distance += self.move_increment
-        print(self.starting_distance)
+        self.forward(STARTING_MOVE_DISTANCE)
+        print(STARTING_MOVE_DISTANCE)
 
-    def hit(self):
-        """ Will return True if player collided with car"""
+    def move_faster(self):
+        global STARTING_MOVE_DISTANCE
+        STARTING_MOVE_DISTANCE += MOVE_INCREMENT
 
-    # def move_faster(self):
-    #     self.new_distance += MOVE_INCREMENT
-    #     self.forward(self.new_distance)
-    #     print(self.new_distance)
-    #
-
+    def reset_cars(self):
+        self.reset()
 
--- Day 23/main.py
+++ Day 23/main.py
@@ -17,12 +17,15 @@ screen.onkey(player.move, "Up")
 cars = []
 game_is_on = True
 make_car = 6
+level = 1
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
     if make_car % 6 == 0:
         car = CarManager()
         cars.append(car)
+        # make_car += 1
     for car in cars:
         car.move_car()
         # TODO The car object is 20x40, now i measure it like square 20x20. Will try to change it later
@@ -30,12 +33,16 @@ while game_is_on:
             print("hit")
             os.system("pause")
     if player.ycor() >= 280:
-        player.reset()
+        car_temp = CarManager()
+        car_temp.move_faster()
+        cars.append(car_temp)
         for car in cars:
-            pass
-            # TODO we need to increment car speed
-            # car.move_faster()
-
+            car.clear()
+            car.hideturtle()
+        player.reset()
+        cars.clear()
+        car_temp.clear()
+        level += 1
     make_car += 1
 
 
